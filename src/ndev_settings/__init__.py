f2d1cdfc6951b3d16210f8acbb8a98cee6f20e46
Tim Monko <timmonko@gmail.com>
2025-09-02 22:22:14 -0500

    remove registration imports

--- src/ndev_settings/__init__.py
+++ src/ndev_settings/__init__.py
@@ -4,28 +4,18 @@ except ImportError:
     __version__ = "unknown"
 
 from ._settings import Settings
-from .registration import (
-    SettingDefinition,
-    create_dynamic_choice_setting,
-    register_settings,
-)
 
 # Singleton instance
 _settings_instance = None
+
 
 def get_settings() -> Settings:
     """Get the singleton instance of the settings manager."""
     global _settings_instance
     if _settings_instance is None:
         from pathlib import Path
+
         _settings_instance = Settings(
             str(Path(__file__).parent / "ndev_settings.yaml")
         )
     return _settings_instance
-
-__all__ = (
-    "get_settings",
-    "SettingDefinition",
-    "register_settings",
-    "create_dynamic_choice_setting",
-)
